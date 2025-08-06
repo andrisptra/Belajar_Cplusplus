62fb323bba4cdaccad574f88714e65237640c52c
andrisptra <andrisptra30@gmail.com>
2025-08-06 21:53:33 +0800

    runtuh

--- toki/operasi_matriks.cpp
+++ toki/operasi_matriks.cpp
@@ -1,111 +1,95 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 using Matrix = vector<vector<int>>;
 
 // fungsi untuk melakukan refleksi matriks secara horizontal
-void reflectHorizontal(Matrix& mat, int n, int m){
-    for (int i = 0; i < n/2; i++)
-    {
-        swap(mat[i], mat[n-1-i]);
-    }
-    
+void reflectHorizontal(Matrix &mat, int n, int m) {
+  for (int i = 0; i < n / 2; i++) {
+    swap(mat[i], mat[n - 1 - i]);
+  }
 }
 // fungsi untuk mereflekasikan matriks secara vertical
-void reflectVertikal(Matrix& mat, int n, int m){
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m/2; j++)
-        {
-            swap(mat[i][j],mat[i][m-1-j]);
-        }
+void reflectVertikal(Matrix &mat, int n, int m) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m / 2; j++) {
+      swap(mat[i][j], mat[i][m - 1 - j]);
     }
-    
+  }
 }
 // fungsi memutar matriks secara 90 derajat
-void rotate90(Matrix& mat, int& n, int& m){
-    Matrix temp(m, vector<int>(n));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            temp[j][n-1-i] = mat[i][j];
-        }  
+void rotate90(Matrix &mat, int &n, int &m) {
+  Matrix temp(m, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      temp[j][n - 1 - i] = mat[i][j];
     }
-    mat = temp;
-    swap(n,m);
-    
+  }
+  mat = temp;
+  swap(n, m);
 }
 
-void rotate180(Matrix& mat, int n, int m){
-    reflectHorizontal(mat,n,m);
-    reflectVertikal(mat,n,m);
+void rotate180(Matrix &mat, int n, int m) {
+  reflectHorizontal(mat, n, m);
+  reflectVertikal(mat, n, m);
 }
 
-void rotate270(Matrix& mat, int& n, int& m){
-    Matrix temp(m, vector<int>(n));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            temp[m-1-j][i] = mat[i][j];
-        }
-        
+void rotate270(Matrix &mat, int &n, int &m) {
+  Matrix temp(m, vector<int>(n));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      temp[m - 1 - j][i] = mat[i][j];
     }
-    mat = temp;
-    swap(n,m);
-    
+  }
+  mat = temp;
+  swap(n, m);
 }
 
 // prosedure untuk melakukan input matriks
-void input_matriks(int N, int M,  vector<vector<int>>& matriks){
-    for(int i = 0; i < N; i++){
-        vector<int> baris;
-        for(int j = 0; j < M; j++){
-            cin >> matriks[i][j];
-        }
+void input_matriks(int N, int M, vector<vector<int>> &matriks) {
+  for (int i = 0; i < N; i++) {
+    vector<int> baris;
+    for (int j = 0; j < M; j++) {
+      cin >> matriks[i][j];
     }
+  }
 }
 // prosedure untuk melakukan proses cetak matriks
-void cetak_matriks(const Matrix& mat, int n, int m){
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << mat[i][j] << (j == m -1 ? "" : " ");
-        }
-        cout <<endl;
+void cetak_matriks(const Matrix &mat, int n, int m) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cout << mat[i][j] << (j == m - 1 ? "" : " ");
     }
+    cout << endl;
+  }
 }
 
-int main(){
-    int N , M, K;
-    cin >> N >> M >> K;
-    
-    Matrix matriks(N, vector<int>(M));
+int main() {
+  int N, M, K;
+  cin >> N >> M >> K;
 
-    input_matriks(N,M,matriks);
+  Matrix matriks(N, vector<int>(M));
 
-    for (int i = 0; i < K; i++)
-    {
-        string x;
-        cin >> x;
-        if (x == "_") {
-             reflectHorizontal(matriks , N, M);
-        } else if (x == "|") {
-             reflectVertikal(matriks , N, M);
-        } else if (x == "90") {
-             rotate90(matriks , N, M);
-        } else if (x == "180") {
-             rotate180(matriks , N, M);
-        } else if (x == "270") {
-             rotate270(matriks , N, M);
-        }
+  input_matriks(N, M, matriks);
+
+  for (int i = 0; i < K; i++) {
+    string x;
+    cin >> x;
+    if (x == "_") {
+      reflectHorizontal(matriks, N, M);
+    } else if (x == "|") {
+      reflectVertikal(matriks, N, M);
+    } else if (x == "90") {
+      rotate90(matriks, N, M);
+    } else if (x == "180") {
+      rotate180(matriks, N, M);
+    } else if (x == "270") {
+      rotate270(matriks, N, M);
     }
-    
-    cetak_matriks(matriks, N, M);
-    
+  }
+
+  cetak_matriks(matriks, N, M);
 }
--- toki/runtuh.cpp
+++ toki/runtuh.cpp
@@ -1,13 +1,79 @@
 #include <iostream>
 #include <vector>
 
-using Matrix = std::vector<std::vector<int>>;
+using Matrix = std::vector<std::vector<char>>;
 
-int main(){
-    int baris, kolom;
-    std::cin >> baris, kolom;
-
-    Matrix matriks(baris, std::vector<int>(kolom));
-
-    return 0;
+// fungsi input matriks
+void input_matriks(int baris, int kolom, Matrix &matriks) {
+  for (int i = 0; i < baris; i++) {
+    for (int j = 0; j < kolom; j++) {
+      std::cin >> matriks[i][j];
+    }
+  }
 }
+// fungsi cetak matriks
+void cetak_matriks(int baris, int kolom, const Matrix &matriks) {
+  for (int i = 0; i < baris; i++) {
+    for (int j = 0; j < kolom; j++) {
+      std::cout << matriks[i][j];
+    }
+    std::cout << std::endl;
+  }
+}
+
+void runtuhkan(int baris, int kolom, Matrix &matriks) {
+  for (int i = 0; i < kolom; i++) {
+    std::vector<char> temp(baris, '0');
+    int pos = baris - 1;
+    for (int j = baris - 1; j >= 0; --j) {
+      if (matriks[j][i] == '1') {
+        temp[pos] = '1';
+        pos--;
+      }
+    }
+    for (int j = 0; j < baris; j++) {
+      matriks[j][i] = temp[j];
+    }
+  }
+}
+
+int main() {
+  int baris, kolom;
+  std::cin >> baris >> kolom;
+
+  Matrix matriks(baris, std::vector<char>(kolom, '0'));
+  input_matriks(baris, kolom, matriks);
+
+  while (true) {
+    bool ada_baris_penuh = false;
+    for (int i = 0; i < baris; i++) {
+      bool penuh = true;
+      for (int j = 0; j < kolom; j++) {
+        if (matriks[i][j] == '0') {
+          penuh = false;
+          break;
+        }
+      }
+      if (penuh) {
+        ada_baris_penuh = true;
+        for (int j = 0; j < kolom; j++) {
+          matriks[i][j] = '0';
+        }
+      }
+    }
+    if (!ada_baris_penuh) {
+      break;
+    }
+    runtuhkan(baris, kolom, matriks);
+  }
+
+  cetak_matriks(baris, kolom, matriks);
+
+  return 0;
+}
+
+// todo
+// cek seluruh elemen baris, apakah terisi penuh? jika ya, maka kita ubah
+// menjadi 0; cek setiap elemen baris, dan cek apakah baris berikutnya dengan
+// kolom yang sama terisi atau tidak? jika tidak maka pindahkan elemen ke kolom
+// tersebut.
